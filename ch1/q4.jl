20304e9aea33e220843af8e476b86a0f5c9a28da
Kai Xu <xukai921110@gmail.com>
2016-09-10 15:33:09 +0100

    Q1.4 done.

---
+++ ch1/q4.jl
@@ -0,0 +1,40 @@
+function palindrome_permutation(str :: ASCIIString)
+  function char_to_int(ch :: Char)
+    a :: Int = int('a')
+    ch :: Int = int(ch)
+    if a <= ch <= int('z')
+      ch - a
+    else
+      -1
+    end
+  end
+
+  function toggle_bit(bit_vector :: Int, idx :: Int)
+    if idx <= 0
+      return bit_vector
+    end
+    bit_vector $= 1 << idx
+  end
+
+  function create_bit_vector(str :: ASCIIString)
+    bit_vector :: Int = 0
+    for ch in str
+      bit_vector = toggle_bit(bit_vector, char_to_int(ch))
+    end
+    bit_vector
+  end
+
+  function has_one_bit(bit_vector :: Int)
+    ((bit_vector - 1) & bit_vector) == 0
+  end
+
+  bv = create_bit_vector(lowercase(str))
+
+  (bv == 0) || has_one_bit(bv)
+end
+
+
+using Base: Test
+
+@test palindrome_permutation("Tact Coa") == true
+@test palindrome_permutation("Tact Coad") == false
