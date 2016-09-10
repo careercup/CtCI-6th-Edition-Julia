390aaae7ba431740c967052f1d9443b000bd4c69
Kai Xu <xukai921110@gmail.com>
2016-09-10 13:10:47 +0100

    Finish LinkedList.

--- DataStructure.jl
+++ DataStructure.jl
@@ -1,22 +1,63 @@
-module DataStructure
-
-export Node, LinkedList, add, ArrayList, HashTable
-
 type Node{T}
     item :: T
     next :: Any
 end
 
-type LinkedList{T}
+type LinkedList
     head :: Any
 end
 
-function add(ll :: LinkedList, item)
-    current = ll.head
-    while current != nothing
-        current = current.next
+function add_to!(ll :: LinkedList, item)
+    if ll.head == nothing
+      ll.head = Node(item, nothing)
+    else
+      curr = ll.head
+      while curr.next != nothing
+          curr = curr.next
+      end
+      curr.next = Node(item, nothing)
     end
-    current.next = Node{T}(item, nothing)
 end
 
+function is_there(ll :: LinkedList, item)
+  curr = ll.head
+  while curr.next != nothing && curr.item != item
+      curr = curr.next
+  end
+  curr.item == item
+end
+
+function remove_from!(ll :: LinkedList, item)
+  @assert ll.head != nothing "[remove_from!] cannot remove an item from an empty LinkedList"
+  prev = nothing
+  curr = ll.head
+  while curr.next != nothing && curr.item != item
+    prev = curr
+    curr = curr.next
+  end
+  if curr.item == item
+    if prev != nothing
+      if curr.next != nothing
+        prev.next = curr.next
+      else
+        prev.next = nothing
+      end
+    else
+      if curr.next != nothing
+        ll.head = curr.next
+      else
+        ll.head = nothing
+      end
+    end
+  end
+end
+
+function Base.string(ll :: LinkedList)
+  str = "["
+  curr = ll.head
+  while curr != nothing
+    str = string(str, " ", curr.item)
+    curr = curr.next
+  end
+  str = string(str, " ]")
 end
--- test_DataStructure.jl
+++ test_DataStructure.jl
@@ -1,4 +1,18 @@
-using DataStructure
+using Base.Test
 
-ll = LinkedList{Int64}(nothing)
-add(ll, 10)
+include("DataStructure.jl")
+
+ll = LinkedList(nothing)
+add_to!(ll, 10)
+add_to!(ll, 1)
+add_to!(ll, 4)
+add_to!(ll, 16)
+@test is_there(ll, 16) == true
+@test string(ll) == "[ 10 1 4 16 ]"
+remove_from!(ll, 10)
+remove_from!(ll, 16)
+remove_from!(ll, 1)
+remove_from!(ll, 4)
+@test string(ll) == "[ ]"
+
+println("All tests of DataStructure.jl pass.")
