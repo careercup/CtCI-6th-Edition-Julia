1113eea76c988a8417964c7e7a5c1c3f6cf11e1c
Kai Xu <xukai921110@gmail.com>
2016-09-10 13:35:56 +0100

    Q1.1 done.

--- ch1/q1.jl
+++ ch1/q1.jl
@@ -1,2 +1,18 @@
-using DataStructure
-println(1)
+function unique_chars(str :: ASCIIString)
+  d = Dict{Char, Int}()
+  for ch in str
+    if haskey(d, ch)
+      return false
+    else
+      d[ch] = 1
+    end
+  end
+  return true
+end
+
+using Base: Test
+
+@test unique_chars("ab *D") == true
+@test unique_chars(" asd+_)(*&^%#@!123456789") == true
+@test unique_chars("abcdc") == false
+@test unique_chars("abcd  ") == false
